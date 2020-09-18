dad305a136286b99e246af99087f705d256d48f2
cmclinton <charles.clinton@gmail.com>
2020-09-18 10:25:32 -0400

    Update ganttrify.R

--- R/ganttrify.R
+++ R/ganttrify.R
@@ -28,34 +28,36 @@
 ganttrify <- function(project,
                       spots = NULL,
                       by_date = FALSE,
-                      exact_date = FALSE, 
+                      exact_date = FALSE,
                       project_start_date = Sys.Date(),
                       colour_palette = wesanderson::wes_palette("Darjeeling1"),
                       font_family = "sans",
                       mark_quarters = FALSE,
                       mark_years = FALSE,
-                      size_wp = 6, 
+                      size_wp = 6,
                       size_activity = 4,
                       size_text_relative = 1,
                       month_number = TRUE,
-                      colour_stripe = "lightgray") {
-  
+                      colour_stripe = "lightgray",
+                      todays_date = TRUE,
+                      line_ends = "butt") {
+
   # repeat colours if not enough colours given
   if (length(unique(project$wp))>length(as.character(wesanderson::wes_palette("Darjeeling1")))) {
     colour_palette <- rep(colour_palette, length(unique(project$wp)))[1:length(unique(project$wp))]
   }
-  
+
   if (by_date==FALSE) {
-    df <- project %>% 
+    df <- project %>%
       dplyr::mutate(start_date = as.numeric(start_date),
                     end_date = as.numeric(end_date))
-    
+
     if (sum(is.na(df$start_date))==nrow(df)) {
       stop("Make sure that the input data are properly formatted and/or you have selected the right paramaters.")
     }
-    
+
     start_yearmon <- zoo::as.yearmon(project_start_date)-(1/12)
-    
+
     df_yearmon <- df %>%
       dplyr::mutate(start_date_yearmon = start_yearmon+(1/12)*start_date,
                     end_date_yearmon = start_yearmon+(1/12)*zoo::as.yearmon(end_date)) %>%
@@ -67,59 +69,59 @@ ganttrify <- function(project,
     if (exact_date==TRUE) {
       #do nothing
     } else {
-      df_yearmon <- project %>% 
+      df_yearmon <- project %>%
         dplyr::mutate(start_date_yearmon = zoo::as.yearmon(start_date),
-                      end_date_yearmon = zoo::as.yearmon(end_date)) %>% 
+                      end_date_yearmon = zoo::as.yearmon(end_date)) %>%
         dplyr::transmute(wp = as.character(wp),
                          activity = as.character(activity),
                          start_date = zoo::as.Date(start_date_yearmon, frac = 0),
                          end_date = zoo::as.Date(end_date_yearmon, frac = 1))
     }
   }
-  
+
   if (exact_date==TRUE) {
-    df <-  project %>% 
+    df <-  project %>%
       dplyr::mutate(start_date = as.Date(start_date),
                     end_date = as.Date(end_date),
                     wp = as.character(wp),
                     activity = as.character(activity))
-    
-    df_yearmon <- df %>% 
+
+    df_yearmon <- df %>%
       dplyr::mutate(start_date = zoo::as.Date(zoo::as.yearmon(start_date), frac = 0),
                     end_date = zoo::as.Date(zoo::as.yearmon(end_date), frac = 1))
-  } 
+  }
 
-  
+
   sequence_months <- seq.Date(from = min(df_yearmon[["start_date"]]),
                               to = max(df_yearmon[["end_date"]]),
                               by = "1 month")
-  
+
   if (length(sequence_months) %% 2 != 0) {
     sequence_months <- seq.Date(from = min(df_yearmon[["start_date"]]),
                                 to = max(df_yearmon[["end_date"]])+1,
                                 by = "1 month")
   }
-  
+
   date_range_matrix <- matrix(as.numeric(sequence_months),
                               ncol = 2,
                               byrow = TRUE)
-  
+
   date_range_df <- tibble::tibble(start = zoo::as.Date.numeric(date_range_matrix[,1]),
                                   end = zoo::as.Date.numeric(date_range_matrix[,2]))
-  
+
   # date breaks in the middle of the month
   date_breaks <- zoo::as.Date(zoo::as.yearmon(seq.Date(from = min(df_yearmon[["start_date"]]+15),
                                                        to = max(df_yearmon[["end_date"]]+15),
                                                        by = "1 month")), frac = 0.5)
-  
+
   date_breaks_q <- seq.Date(from = lubridate::floor_date(x = min(df_yearmon[["start_date"]]), unit = "year"),
                             to = lubridate::ceiling_date(x = max(df_yearmon[["end_date"]]), unit = "year"),
                             by = "1 quarter")
-  
+
   date_breaks_y <- seq.Date(from = lubridate::floor_date(x = min(df_yearmon[["start_date"]]), unit = "year"),
                             to = lubridate::ceiling_date(x = max(df_yearmon[["end_date"]]), unit = "year"),
                             by = "1 year")
-  
+
   df_levels <- rev(df_yearmon %>%
                      dplyr::select(wp, activity) %>%
                      t() %>%
@@ -133,7 +135,7 @@ ganttrify <- function(project,
                          dplyr::summarise(activity = unique(wp), start_date = min(start_date), end_date = max(end_date)), .id = "type") %>%
       dplyr::mutate(activity = factor(x = activity, levels = df_levels)) %>%
       dplyr::arrange(activity)
-    
+
   } else {
     df_yearmon_fct <-
       dplyr::bind_rows(activity = df_yearmon,
@@ -143,9 +145,9 @@ ganttrify <- function(project,
       dplyr::mutate(activity = factor(x = activity, levels = df_levels)) %>%
       dplyr::arrange(activity)
   }
-  
-  
-  
+
+
+
   gg_gantt <- ggplot2::ggplot(data = df_yearmon_fct,
                               mapping = ggplot2::aes(x = start_date,
                                                      y = activity,
@@ -159,29 +161,37 @@ ganttrify <- function(project,
                                                           ymax = Inf),
                        inherit.aes=FALSE,
                        alpha = 0.4,
-                       fill = colour_stripe) 
-  
+                       fill = colour_stripe)
+
   if (mark_quarters == TRUE) {
     gg_gantt <- gg_gantt +
       ggplot2::geom_vline(xintercept = date_breaks_q, colour = "gray50")
   }
-  
+
   if (mark_years == TRUE) {
     gg_gantt <- gg_gantt +
       ggplot2::geom_vline(xintercept = date_breaks_y, colour = "gray50")
   }
-  
+
+  if (todays_date == TRUE){
+    gg_gantt <- gg_gantt +
+      ggplot2::geom_vline(xintercept = Sys.Date(),color="grey50")
+  }
+
+
+
+
   gg_gantt <- gg_gantt +
     ### activities
     ggplot2::geom_segment(data = df_yearmon_fct,
-                          lineend = "round",
+                          lineend = line_ends,
                           size = size_activity) +
     ### wp
     ggplot2::geom_segment(data = df_yearmon_fct %>%
                             dplyr::filter(type=="wp"),
-                          lineend = "round",
+                          lineend = line_ends,
                           size = size_wp) 
-  
+
   if (month_number==TRUE) {
     gg_gantt <- gg_gantt +
       ggplot2::scale_x_date(name = "",
@@ -196,7 +206,7 @@ ganttrify <- function(project,
                               date_labels = "%b\n%Y",
                               minor_breaks = NULL)
   }
-  
+
   gg_gantt <- suppressWarnings(gg_gantt +
     ggplot2::scale_y_discrete("") +
     ggplot2::theme_minimal() +
@@ -211,48 +221,47 @@ ganttrify <- function(project,
   if (is.null(spots)==FALSE) {
     if (is.data.frame(spots)==TRUE) {
       if (by_date==FALSE) {
-        spots_date <- spots %>% 
-          tidyr::drop_na() %>% 
-          dplyr::mutate(spot_date = as.numeric(spot_date), 
+        spots_date <- spots %>%
+          tidyr::drop_na() %>%
+          dplyr::mutate(spot_date = as.numeric(spot_date),
                         activity = as.character(activity),
-                        spot_type = as.character(spot_type)) %>% 
-          dplyr::mutate(activity = factor(x = activity, levels = df_levels), 
-                        spot_date = zoo::as.Date(start_yearmon+(1/12)*zoo::as.yearmon(spot_date), frac = 0.5), 
-                        end_date = as.Date(NA), 
+                        spot_type = as.character(spot_type)) %>%
+          dplyr::mutate(activity = factor(x = activity, levels = df_levels),
+                        spot_date = zoo::as.Date(start_yearmon+(1/12)*zoo::as.yearmon(spot_date), frac = 0.5),
+                        end_date = as.Date(NA),
                         wp = NA)
       } else {
         if (exact_date==TRUE) {
-          spots_date <- spots %>% 
-            tidyr::drop_na() %>% 
-            dplyr::mutate(activity = as.character(activity)) %>% 
-            dplyr::mutate(activity = factor(x = activity, levels = df_levels), 
-                          spot_date = as.Date(spot_date), 
-                          end_date = as.Date(NA), 
+          spots_date <- spots %>%
+            tidyr::drop_na() %>%
+            dplyr::mutate(activity = as.character(activity)) %>%
+            dplyr::mutate(activity = factor(x = activity, levels = df_levels),
+                          spot_date = as.Date(spot_date),
+                          end_date = as.Date(NA),
                           wp = NA)
         } else {
-          spots_date <- spots %>% 
-            tidyr::drop_na() %>% 
-            dplyr::mutate(activity = factor(x = activity, levels = df_levels), 
-                          spot_date = zoo::as.Date(zoo::as.yearmon(spot_date), frac = 0.5), 
-                          end_date = as.Date(NA), 
+          spots_date <- spots %>%
+            tidyr::drop_na() %>%
+            dplyr::mutate(activity = factor(x = activity, levels = df_levels),
+                          spot_date = zoo::as.Date(zoo::as.yearmon(spot_date), frac = 0.5),
+                          end_date = as.Date(NA),
                           wp = NA)
         }
       }
 
-      
+
       gg_gantt <- gg_gantt +
-        ggplot2::geom_label(data = spots_date, 
+        ggplot2::geom_label(data = spots_date,
                             mapping = ggplot2::aes(x = spot_date,
                                                    y = activity,
                                                    label = spot_type),
                             colour = "gray30",
                             fontface = "bold",
                             family = font_family,
-                            size = 3*size_text_relative) 
+                            size = 3*size_text_relative)
     }
-  } 
-  
-  return(gg_gantt)
-  
-}
+  }
 
+  return(gg_gantt)
+
+}
